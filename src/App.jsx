36afbc21cc3de2067354e3042b1e573f0a6ef4c3
satyam <mishrasatyamsam@gmail.com>
2025-01-10 17:20:03 +0530

    routing setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Body from "./components/Body";
+import Profile from "./components/Profile";
 
 function App() {
-
   return (
     <>
-      <h1>Hello World</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
