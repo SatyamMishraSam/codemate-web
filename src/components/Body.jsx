9eee3f572c0c4491ef167dfd1ca10e6831e2991a
satyam <mishrasatyamsam@gmail.com>
2025-01-10 21:58:27 +0530

    auth, protected routes done

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+
+// why fetchUser =>
+// we have to login to access / page
+// Now we can't access any page without login
+
+// whenever we login => our creds => store
+// but if we refresh our page then its getting loggedout  but still my token is there in the cookie
+// our main page is body , onload => we will fetch whethere user is there or not and that can be done with /profile page
+// so everytime our page (body) gets loaded we will make a call to /profile and if token is there then we will get the details of user and put to store
+
+// if token is not there then will say to login
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (e) {
+      navigate("/login");
+
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <>
       <Navbar />
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,83 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("virat@gmail.com");
+  const [password, setPassword] = useState("Virat@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        {
+          email,
+          password,
+        },
+        { withCredentials: true }
+      );
+
+      dispatch(addUser(res.data));
+      toast.success("Login successful!", {
+        position: "top-center",
+      });
+      setTimeout(() => navigate("/"), 1000);
+    } catch (e) {
+      console.log(e);
+      toast.error("Login failed! Please check your credentials.", {
+        position: "top-center",
+      });
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Email ID {email} </span>
+              </div>
+              <input
+                type="text"
+                placeholder=""
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Password {password} </span>
+              </div>
+              <input
+                type="text"
+                placeholder=""
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-center m-2">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+            <ToastContainer position="top-center" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
